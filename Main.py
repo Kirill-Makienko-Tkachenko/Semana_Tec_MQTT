aaf66bd1d6306982e37b89bd709c01c2a9f3c4ab
DulceBucio <dulcenahomibucio@gmail.com>
2023-05-11 14:20:41 -0600

    Code correctly commented

--- Main.py
+++ Main.py
@@ -1,26 +1,24 @@
 #Hecho por Kirill Makienko Tkachenko
-#10/06/23
+#10/05/23
 #Parte del codigo se encuentra en: https://www.emqx.com/en/blog/how-to-use-mqtt-in-python
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-#Librerias utilizadas
+
+# Used libraries
 import sys
 from paho.mqtt import client as mqtt_client
 import random
 import time
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-#Constantes
-port = 1883 #El puerto siempre es constante debido a las intrucciones del profesor
-client_id = f'python-mqtt-{random.randint(0, 1000)}' #Asigna un id al cleinte que envia o subscribe de manera automatica
 
-#py Main.py send -c Marco -m hola -p 52.23.241.126
-#py Main.py subscribe -c Marco -p 52.23.241.126
+# Constants 
+port = 1883 # Port that we will always use
+client_id = f'python-mqtt-{random.randint(0, 1000)}' # Randomized ID for the client
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-#Funcion utilizada para asignar valores sin importar el orden en el que se encuentren en la linea de comandos
-
+# Function to assign values despite the order of the command line
 def descifrar_orden(argv):
     for i in range(len(argv)-1):
         
@@ -40,30 +38,27 @@ def descifrar_orden(argv):
 
  #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-#Funciones que habilitan el push del mensaje
-
-#Funcion que conecta el cliente al IP
+# Function that connects the client to the IP
 def connect_mqtt(broker):
-    def on_connect(client, userdata, flags, rc): #Funcion que confirma conexion exitosa
-        
+    # Function that return the connection state
+    def on_connect(
+            client, userdata, flags, 
+            rc): 
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id) #Asigna Client Id a la maquina que se conecta
-    # client.username_pw_set(username, password)
+    client = mqtt_client.Client(client_id) # Assigns Client ID 
     client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    client.connect(broker, port) # Establish the connection with the server
+    return client 
 
-#Funcion que publica un mensaje (Push en un canal, indicado en CLI) 
+# Function to publish a message in a topic
 def publish(client, topic, msg):
     msg_count = 0
     while True:
-        #time.sleep(1)
         result = client.publish(topic, msg)
-        # result: [0, 1]
         status = result[0]
         if status == 0:
             print(f"Send `{msg}` to topic `{topic}`")
@@ -75,25 +70,24 @@ def publish(client, topic, msg):
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-#Funciones que habilitan suscribirse a un topico de un canal
+# Functions that enable suscribing to a channel 
 
-#Funcion que apunta a connect_mqtt para conectar
+# Function that connects to the mqtt client 
 def connect_mqtt(broker) -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(
+            client, userdata, flags, 
+            rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
-
-#Funcion para suscribir el cliente y recibir mensajes
-
+# Function to suscribe the client and receive messages
 def subscribe(client: mqtt_client, topic):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
@@ -105,24 +99,23 @@ def subscribe(client: mqtt_client, topic):
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-#Programa Main
+# Main program
 
-#Revisa si el argumento pasado del CLI es send o subscribe
+# Verifies the client choice in order to send or receive message
 if sys.argv[1] == "send":
     broker,topic,message = descifrar_orden(sys.argv)
     print(broker)
-    while True:#Permite enviar mensajes de manera perpetua hasta que se cancele
-
-        client = connect_mqtt(broker)
-        publish(client, topic, message)
-        message = input("Cual es tu mensaje (Apaga la consola para salir): ")
+    while True: # Allows to keep sending messages 
+        client = connect_mqtt(broker) # Establish the connection 
+        publish(client, topic, message) # Publish the message to the topic
+        message = input("Cual es tu mensaje (Apaga la consola para salir): ") # Receives the message to publish 
 
 
 elif sys.argv[1] == "subscribe":
-    broker,topic = descifrar_orden(sys.argv)
-    client = connect_mqtt(broker)
-    subscribe(client, topic)
-    client.loop_forever()#Permite que el cliente de subscibcion este constantemente refrescandose
+    broker,topic = descifrar_orden(sys.argv) 
+    client = connect_mqtt(broker) # Establish the connection
+    subscribe(client, topic) # Receive the message
+    client.loop_forever() # Allows the client to keep receiving messages
 
 else:
     print("Opcion invalida\n")
